5bfc0e4cf44a72aa8530dea0550dd9ed24d8f8e6
sachin7con <sachin7con@gmail.com>
2024-11-23 17:09:16 +0530

    First commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,43 @@
-
+//SGN
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
 import './../styles/App.css';
+
+const Home = () => <h2>Welcome to my website!</h2>;
+const About = () => {
+  return(
+    <div>
+      <h2>About</h2>
+      <p>This page is About my website </p>
+    </div>
+  )
+}
+
+
+const Switch = () => {
+  return(
+    <div>
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        {/*ONS Do not remove the main div */}
+        <Switch />
     </div>
   )
 }
